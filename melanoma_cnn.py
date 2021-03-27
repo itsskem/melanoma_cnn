ba5448815b5f4ae11841c7f8b6ce5fc1759e2bde
Kemka Ihemelandu <kemkaihemelandu@gmail.com>
2021-03-27 12:20:11 -0400

    changed the parameters and added transformation

--- melanoma_cnn.py
+++ melanoma_cnn.py
@@ -5,21 +5,29 @@ import torchvision
 import torchvision.transforms as transforms
 ## import tensorboard
 from torch.utils.tensorboard import SummaryWriter
+from sklearn.metrics import accuracy_score
+
 ### CHANGE ###
-data_path = '174469_505351_bundle_archive\\train'
+data_path = 'DermMel\\train'
+transformations = transforms.Compose([
+    transforms.Resize((224,224)), 
+    transforms.RandomHorizontalFlip(p=0.5),           #resizing all of the pictures
+    torchvision.transforms.ToTensor()
+    ## other transformations? such as horizonal or vertical random flips
+])
 trainset = torchvision.datasets.ImageFolder(                #loading all of the data into the right place
     root=data_path,
-    transform=torchvision.transforms.ToTensor()
-)
+    transform=transformations
+)  #transforms images into a tensor
 ##############
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=6,           #8 data points at a time (every time it loads)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=10,           #8 data points at a time (every time it loads)
                                           shuffle=True, num_workers=0)
 
 ### CHANGE ###
-data_path = '174469_505351_bundle_archive\\test'
+data_path = 'DermMel\\test'
 validset = torchvision.datasets.ImageFolder(
     root=data_path,
-    transform=torchvision.transforms.ToTensor()
+    transform=transformations
 )
 ##############
 validloader = torch.utils.data.DataLoader(validset, batch_size=10,
@@ -56,12 +64,11 @@ net = Net() #creates neural network
 import torch.optim as optim
 
 criterion = nn.CrossEntropyLoss() # cost function
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9) #gradient descent
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.5) #gradient descent
 ## tensorboard --logdir=runs
-writer = SummaryWriter('runs/with12_1') #creates and labels graph for organization
+writer = SummaryWriter('runs/OT_trialrunoftransformation') #creates and labels graph for organization
 iterations = 1
 for epoch in range(50):  # loop over the dataset multiple times
-    running_loss = 0.0
     for i, data in enumerate(trainloader, 0): #enumerate creates trainloader into a "loopable" object #0 keeps count
         # get the inputs; data is a list of [inputs, labels]
         inputs, labels = data
@@ -74,20 +81,28 @@ for epoch in range(50):  # loop over the dataset multiple times
         loss = criterion(outputs, labels) #taking the cost function of the outputs and labels to compare
         loss.backward()
         optimizer.step() #taking the gradient descent
-        
         writer.add_scalar('training loss', loss, iterations) #plots the iterations and loss on the graph as it is training
+
+        y_true = labels.numpy()
+        y_pred = torch.argmax(outputs, dim=1).numpy() # next week
+        accuracy = accuracy_score(y_true, y_pred, normalize=False)  #calculating the accuracy
+        writer.add_scalar('training accuracy', accuracy, iterations)
 
         iterations += 1 #adding 1 to each iteration to help keep track
         print(loss)
+
+
 
         if iterations % 50 == 0:
             ## load a batch of new data (validation data)
             inputs_valid, labels_valid = iter(validloader).next()
             outputs_valid = net(inputs_valid)
             loss_valid = criterion(outputs_valid, labels_valid)
-
             writer.add_scalar('validation loss', loss_valid, iterations)
-
+            y_true = labels_valid.numpy()
+            y_pred = torch.argmax(outputs_valid, dim=1).numpy()
+            val_accuracy = accuracy_score(y_true, y_pred, normalize=False)
+            writer.add_scalar('valication accuracy',  val_accuracy, iterations)
 
             ## put the prediction error on new data into the tensorboard
 
